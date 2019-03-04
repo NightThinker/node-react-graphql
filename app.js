0f61b52e94d1e06dd38d066fd004743a35f0bca2
May <may@Gato-Pro.local>
2019-03-04 11:43:22 +0700

    Schemas & Resolvers

--- app.js
+++ app.js
@@ -1,12 +1,43 @@
 const express = require('express');
 const bodyParser = require('body-parser');
+const graphqlHttp = require('express-graphql')
+const { buildSchema } = require('graphql')
 
 const app = express()
 
 app.use(bodyParser.json())
 
-app.get('/', (req, res, next) => {
-  res.send('Hello')
-})
+app.use(
+  '/graphql', 
+  graphqlHttp({
+    schema: buildSchema(`
+      type RootQuery {
+        events: [String!]!
+      }
 
-app.listen(3000)
+      type RootMutation {
+        createEvent(name: String!): String
+      }
+
+      schema {
+        query: RootQuery
+        mutation: RootMutation
+      }
+    `),
+    rootValue: {
+      events: () => {
+        return ['Romatic Cooking', 'Sailing', 'All-Night Coding']
+      },
+      createEvent: ({ name }, req) => {
+        return name
+      }
+    },
+    graphiql: true
+  })
+)
+
+// app.get('/',(req, res, next) => {
+//   res.send('Hlloe')
+// })
+
+app.listen(4000)
