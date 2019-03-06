e4395923eeef3674820bddcbaafc9b834bbe56c7
May <may@Gato-Pro.local>
2019-03-06 17:25:12 +0700

    Refactoring our Code

---
+++ graphql/resolvers/booking.js
@@ -0,0 +1,37 @@
+const Event = require('../../models/event')
+const Booking = require('../../models/booking')
+const { transformEvent, transformBooking } = require('./merge')
+
+
+module.exports = {
+  bookings: async () => {
+    try {
+      const bookings = await Booking.find() 
+      return bookings.map(booking => {
+        return transformBooking(booking)
+      })
+    } catch(err) {
+      throw err
+    }
+  },
+  bookEvent: async ({ eventId }) => {
+    const fetchedEvent = await Event.findOne({ _id: eventId })
+    const booking = new Booking({
+      user: '5c7ded9f2bfcc70b98f9dffd',
+      event: fetchedEvent
+    });
+    const result = await booking.save()
+    return transformBooking(result)
+  },
+  cancelBooking: async ({ bookingId }) => {
+    try {
+      const booking = await Booking.findById(bookingId) .populate('event')
+      const event = transformEvent(booking._doc.event)
+      await Booking.deleteOne({ _id: bookingId })
+      return event
+    } catch(err) {
+      throw err
+    }
+
+  }
+}
---
+++ graphql/resolvers/events.js
@@ -0,0 +1,42 @@
+const User = require('../../models/user')
+const Event = require('../../models/event')
+const { transformEvent } = require('./merge')
+
+
+module.exports = {
+  events: async () => {
+    try{
+      const events = await Event.find()
+      return events.map(event => {
+        return transformEvent(event)
+      })
+    } catch(err) {
+        throw err
+    }
+  },
+  createEvent: async ({ eventInput }) => {
+    const event = new Event({
+      title: eventInput.title,
+      description: eventInput.description,
+      price: +eventInput.price,
+      date: new Date(),
+      creator: '5c7ded9f2bfcc70b98f9dffd'
+    })
+    let createdEvent;
+    try{
+      const result = await event.save()
+      createdEvent =  transformEvent(result)
+      const creator = await User.findById('5c7ded9f2bfcc70b98f9dffd')
+      
+      if(!creator) {
+        throw new Error('User not found.')
+      }
+      creator.createdEvents.push(event)
+      await creator.save()
+      return createdEvent
+
+    } catch(err) {
+      throw err
+    }
+  },
+}
